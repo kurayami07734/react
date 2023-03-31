ae6d2e8ac2ea54b36a2fbaf72e2284cd54bb9186
Aditya Ghidora <aditya@ghidora.tech>
2023-03-31 23:13:02 +0530

    starting ssr with react-router

--- using-jsx/src/App.jsx
+++ using-jsx/src/App.jsx
@@ -1,27 +1,35 @@
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import SearchParams from "./SearchParams";
 import AdoptedPetContext from "./AdoptedPetContext";
-import Details from "./Details";
-import { useState } from "react";
+import { useState, lazy, Suspense } from "react";
 const queryClient = new QueryClient({
   defaultOptions: { queries: { staleTime: Infinity, cacheTime: Infinity } },
 });
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 function App() {
   const adoptedPetHook = useState(null);
   return (
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <AdoptedPetContext.Provider value={adoptedPetHook}>
-          <header>
-            <Link to="/">Adopt me!</Link>
-          </header>
-          <Routes>
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="/" element={<SearchParams />} />
-          </Routes>
-        </AdoptedPetContext.Provider>
-      </QueryClientProvider>
+      <AdoptedPetContext.Provider value={adoptedPetHook}>
+        <QueryClientProvider client={queryClient}>
+          <Suspense
+            fallback={
+              <div className="loading-pane">
+                <h2 className="loader">Loading</h2>
+              </div>
+            }
+          >
+            <header>
+              <Link to="/">Adopt me!</Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </Suspense>
+        </QueryClientProvider>
+      </AdoptedPetContext.Provider>
     </BrowserRouter>
   );
 }
